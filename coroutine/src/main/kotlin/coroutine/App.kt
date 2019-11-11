d625ebd11215ce67d56553ef37690a974f2ff9c5
yuu ogasawara <performa532@gmail.com>
2019-11-11 18:58:18 +0900

    add example
    [Your first coroutine with Kotlin](https://kotlinlang.org/docs/tutorials/coroutines/coroutines-basic-jvm.html#setting-up-a-project)

--- coroutine/src/main/kotlin/coroutine/App.kt
+++ coroutine/src/main/kotlin/coroutine/App.kt
@@ -3,11 +3,15 @@
  */
 package coroutine
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicLong
 
 fun main(args: Array<String>) {
+//    secondExample()
+    thirdExample()
+}
+
+fun firstExample() {
     println("Start")
 
     // Start a coroutine
@@ -16,7 +20,12 @@ fun main(args: Array<String>) {
         println("Hello")
     }
 
-    Thread.sleep(2000) // wait for 2 seconds
+    runBlocking {
+        delay(2000)
+    }
+//    Thread.sleep(2000) // wait for 2 seconds
+    // if you comment out sleep, println("Hello") will not execute.
+    // because the coroutine and delay function are non-blocking.
     println("Stop")
 
     /*実行結果：
@@ -25,4 +34,46 @@ fun main(args: Array<String>) {
       end
     *
     * */
+}
+
+fun secondExample() {
+//    val c = AtomicLong()
+//
+//    for (i in 1..1_000_000L)
+//        GlobalScope.launch {
+//            c.addAndGet(i)
+//        }
+//
+//    println(c.get())
+
+    val deferred : List<Deferred<Int>> = (1..1_000_000).map { n ->
+        GlobalScope.async {
+            delay(1000)//does not run over 1,000,000 seconds because this run in parallel.
+            n * 2
+        }
+    }
+
+    runBlocking {
+        val sum = deferred.sumBy { it.await() }
+        //See the results in coroutine
+        println("Sum: $sum")
+    }
+}
+
+fun thirdExample() {
+    suspend fun workload(n: Int): Int {
+        delay(1000)
+        return n
+    }
+
+    val deferred : List<Deferred<Int>> = (1..1_000_000).map { n ->
+        GlobalScope.async {
+            workload(n)
+        }
+    }
+    runBlocking {
+        val sum = deferred.sumBy { it.await() }
+        //See the results in coroutine
+        println("Sum: $sum")
+    }
 }
